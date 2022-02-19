f354d1d0748e9b30ff48664784a9fee0c3bc0d52
John <gtom_1984@hotmail.ca>
2022-02-18 17:44:38 -0800

    Add Flair page
    - may need to do some customizations and stuff

--- app/src/main/java/com/example/sfuquizlet/EditCardPage.kt
+++ app/src/main/java/com/example/sfuquizlet/EditCardPage.kt
@@ -6,15 +6,20 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.InputFilter
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.GridLayout
 import android.widget.TextView
 import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.sfuquizlet.databinding.ActivityEditCardPageBinding
+import java.io.Console
 
 lateinit var binding : ActivityEditCardPageBinding
 class EditCardPage : AppCompatActivity() {
@@ -25,9 +30,10 @@ class EditCardPage : AppCompatActivity() {
         setContentView(view)
 
         var test = arrayOf(
-            ViewHelperClass("Edit Question", "Enter text", emptyArray<String>()),
-            ViewHelperClass("Edit Card", "Enter text", emptyArray<String>())
-            )
+            EditCardHelperClass("Edit Question", "Enter text", ArrayList<String>()),
+            EditCardHelperClass("Edit Card", "Enter text", ArrayList<String>()),
+            EditCardHelperClass("Flairs", "Enter Flair", ArrayList<String>(arrayListOf("Midterm 1", "Quiz", "Neuroptics")))
+        )
 
         var recyclerView = binding.recycler
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -35,11 +41,11 @@ class EditCardPage : AppCompatActivity() {
     }
 }
 
-data class ViewHelperClass(val title: String, var text: String, var textArray: Array<String>)
+data class EditCardHelperClass(val title: String, var text: String, var textArray: ArrayList<String>)
 
 
 // For the whole view
-class EditCardRecycler(val array: Array<ViewHelperClass>,
+class EditCardRecycler(val array: Array<EditCardHelperClass>,
                        context: Context,
                        layoutInflater: LayoutInflater) : RecyclerView.Adapter<RecyclerView.ViewHolder> () {
     val flairId = 0;
@@ -60,28 +66,82 @@ class EditCardRecycler(val array: Array<ViewHelperClass>,
             }
     }
 
+    class FlairEditorHolder(val view : View, val context: Context)
+        : RecyclerView.ViewHolder(view) {
+
+        fun setFlairList(flairs : ArrayList<String>) {
+            val flairList = this.itemView.findViewById<RecyclerView>(R.id.FlairList)
+            flairList.layoutManager = object : StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL) {
+                override fun canScrollHorizontally(): Boolean {
+                    return false
+                }
+
+                override fun canScrollVertically(): Boolean {
+                    return false
+                }
+            }
+            flairList.adapter = FlairViewReycler(flairs, context, LayoutInflater.from(context))
+        }
+
+        fun setInput() {
+            val inputField = this.itemView.findViewById<EditText>(R.id.FlairNameInput)
+            val submit = this.itemView.findViewById<TextView>(R.id.FlairSubmit)
+            val flairList = this.itemView.findViewById<RecyclerView>(R.id.FlairList)
+            submit.setOnClickListener {
+                Log.d("Flair add", "adding " + inputField.text.toString())
+                (flairList.adapter as FlairViewReycler).addItem(
+                    inputField.text.toString()
+                )
+            }
+        }
+    }
+
     override fun getItemViewType(position: Int): Int {
-        if(position == itemCount - 1){
+        if(position == array.size - 1){
             return flairId;
         }
         return editFieldID
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val inflator = LayoutInflater.from(parent.context)
-        val layoutParams: ViewGroup.LayoutParams = ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            800
-        )
-        val view = inflator.inflate(R.layout.fill_in_card, parent, false)
-        view.layoutParams = layoutParams
-        return FillInHolder(view, 250)
+        when(viewType) {
+            editFieldID -> {
+                val inflator = LayoutInflater.from(parent.context)
+                val layoutParams: ViewGroup.LayoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    800
+                )
+                val view = inflator.inflate(R.layout.fill_in_card, parent, false)
+                view.layoutParams = layoutParams
+                return FillInHolder(view, 250)
+            }
+            else -> {
+                val inflator = LayoutInflater.from(parent.context)
+                val layoutParams: ViewGroup.LayoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+                val view = inflator.inflate(R.layout.flair_input, parent, false)
+                view.layoutParams = layoutParams
+                return FlairEditorHolder(view, parent.context)
+            }
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val fillInHolder = (holder as FillInHolder)
-        fillInHolder.setContent(array[position].text)
-        fillInHolder.setTitle(array[position].title)
+        when(holder.itemViewType) {
+            editFieldID -> {
+                val fillInHolder = (holder as FillInHolder)
+                fillInHolder.setContent(array[position].text)
+                fillInHolder.setTitle(array[position].title)
+            }
+            else -> {
+                val editFlairHolder = (holder as FlairEditorHolder)
+                editFlairHolder.setFlairList(array[position].textArray)
+                editFlairHolder.setInput()
+            }
+        }
+
     }
 
     override fun getItemCount(): Int = array.size
--- app/src/main/java/com/example/sfuquizlet/FlairViewReycler.kt
+++ app/src/main/java/com/example/sfuquizlet/FlairViewReycler.kt
@@ -4,22 +4,48 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-// For the flair page
-class FlairViewReycler(val array: Array<String>, context: Context, layoutInflater: LayoutInflater) :
-    RecyclerView.Adapter<FlairViewReycler.FlairViewHolder>() {
+interface Remover {
+    fun onRemove()
+}
 
-    class FlairViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+// For the flair page
+class FlairViewReycler(val array: ArrayList<String>, context: Context, layoutInflater: LayoutInflater) :
+    RecyclerView.Adapter<FlairViewReycler.FlairViewHolder>(){
+
+    class FlairViewHolder(val view: View, pos: Int) : RecyclerView.ViewHolder(view){
+        var pos : Int
+            get() {
+                return pos
+            }
+            set(value) {
+                pos = value
+            }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlairViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.flair_button, parent, false)
+        return FlairViewHolder(view, 0)
     }
 
     override fun onBindViewHolder(holder: FlairViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
+        val view = holder.itemView.findViewById<View>(R.id.FlairDelete)
+        val text = holder.itemView.findViewById<TextView>(R.id.FlairText)
+        text.text = array[position]
+        view.setOnClickListener {
+            array.removeAt(position)
+            this.notifyDataSetChanged()
+        }
 
+    }
     override fun getItemCount() = array.size
+
+    fun addItem(flairName: String) {
+        array.add(flairName)
+        notifyItemInserted(array.size - 1);
+    }
 
 }
