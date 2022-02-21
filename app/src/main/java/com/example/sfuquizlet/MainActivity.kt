b4c4f087615d249a303178f36b3f56cdade94ea7
Kiaan Castillo <kiaan_castillo@sfu.ca>
2022-02-20 21:34:28 -0800

    Navbar working, only profile navbar item works

--- app/src/main/java/com/example/sfuquizlet/MainActivity.kt
+++ app/src/main/java/com/example/sfuquizlet/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.sfuquizlet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navBarHomeButton = findViewById<ImageView>(R.id.navBarHomeID)
+        navBarHomeButton.setOnClickListener {
+            // TODO
+        }
+
+        val navBarCoursesButton = findViewById<ImageView>(R.id.navBarCoursesID)
+        navBarCoursesButton.setOnClickListener {
+            // TODO
+        }
+
+        val navBarProfileButton = findViewById<ImageView>(R.id.navBarProfileID)
+        navBarProfileButton.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragmentContainerViewID, EditProfileFragment())
+            transaction.commit()
+        }
+
     }
 }
