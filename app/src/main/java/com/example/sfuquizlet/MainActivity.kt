b75a77c3c3467484c2212f8899ea2092b2b51cf8
Kiaan Castillo <kiaan_castillo@sfu.ca>
2022-02-20 22:28:01 -0800

    Navbar buttons functionality

--- app/src/main/java/com/example/sfuquizlet/MainActivity.kt
+++ app/src/main/java/com/example/sfuquizlet/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.sfuquizlet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
@@ -9,22 +12,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val layoutInflator = LayoutInflater.from(this)
+        val frameLayout = findViewById<FrameLayout>(R.id.frameLayoutID)
+        val dashBoardView = layoutInflator.inflate(R.layout.dashboard, null)
+        frameLayout.addView(dashBoardView)
+
         val navBarHomeButton = findViewById<ImageView>(R.id.navBarHomeID)
         navBarHomeButton.setOnClickListener {
-            // TODO
+            frameLayout.removeAllViews()
+            frameLayout.addView(layoutInflator.inflate(R.layout.dashboard, null))
         }
 
         val navBarCoursesButton = findViewById<ImageView>(R.id.navBarCoursesID)
         navBarCoursesButton.setOnClickListener {
-            // TODO
+            frameLayout.removeAllViews()
+            frameLayout.addView(layoutInflator.inflate(R.layout.course_list, null))
         }
 
         val navBarProfileButton = findViewById<ImageView>(R.id.navBarProfileID)
         navBarProfileButton.setOnClickListener {
-            val transaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragmentContainerViewID, EditProfileFragment())
-            transaction.commit()
+            frameLayout.removeAllViews()
+            frameLayout.addView(layoutInflator.inflate(R.layout.fragment_edit_profile, null))
         }
-
     }
 }
