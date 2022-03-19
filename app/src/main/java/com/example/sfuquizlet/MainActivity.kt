5661f73dd85a5b5eff910fc2f11b2618b87f0fb6
Taha Liaqat <tliaqat@sfu.ca>
2022-03-19 10:45:45 -0700

    refactored code

--- app/src/main/java/com/example/sfuquizlet/MainActivity.kt
+++ app/src/main/java/com/example/sfuquizlet/MainActivity.kt
@@ -6,8 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
-import com.example.sfuquizlet.database.getCardsFromDatabase
-import com.example.sfuquizlet.database.getFlairsFromDatabase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -26,6 +24,10 @@ class MainActivity : AppCompatActivity() {
         val database = Firebase.database
     }
 
+    lateinit var  layoutInflator: LayoutInflater
+    lateinit var  frameLayout: FrameLayout
+    lateinit var dashBoardView: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,22 +35,28 @@ class MainActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         login(this) // SignInFragment.kt
 
-//        seedDatabase()
-//        getAllDecks()
-//        getCardsFromDatabase(mutableListOf("312f0f46-721a-44ac-bf87-1ecef6c5d829", "2b74b63a-ed5c-4c39-b997-93f790d623c6"))
-//        getFlairsFromDatabase(mutableListOf("38642d44-cee1-46b3-85b5-3920f10bd6a5", "7cd6d013-510e-40d4-bb7d-d271d2d68a25"))
 
-        val layoutInflator = LayoutInflater.from(this)
-        val frameLayout = findViewById<FrameLayout>(R.id.frameLayoutID)
-        val dashBoardView = layoutInflator.inflate(R.layout.dashboard, null)
+        layoutInflator = LayoutInflater.from(this)
+        frameLayout = findViewById(R.id.frameLayoutID)
+        dashBoardView = layoutInflator.inflate(R.layout.dashboard, null)
+
         frameLayout.addView(dashBoardView)
 
+        initializeNavBarHomeBtn()
+        initializeNavBarCoursesBtn()
+        initializeNavBarProfileBtn()
+
+    }
+
+    private fun initializeNavBarHomeBtn() {
         val navBarHomeButton = findViewById<ImageView>(R.id.navBarHomeID)
         navBarHomeButton.setOnClickListener {
             frameLayout.removeAllViews()
             frameLayout.addView(layoutInflator.inflate(R.layout.dashboard, null))
         }
+    }
 
+    private fun initializeNavBarCoursesBtn() {
         val navBarCoursesButton = findViewById<ImageView>(R.id.navBarCoursesID)
         navBarCoursesButton.setOnClickListener {
             frameLayout.removeAllViews()
@@ -63,7 +71,9 @@ class MainActivity : AppCompatActivity() {
             }
             frameLayout.addView(courseListView)
         }
+    }
 
+    private fun initializeNavBarProfileBtn() {
         val navBarProfileButton = findViewById<ImageView>(R.id.navBarProfileID)
         navBarProfileButton.setOnClickListener {
             frameLayout.removeAllViews()
