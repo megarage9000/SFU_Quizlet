87cef9feae71e7b322049f59f052ce9c3cb85cc5
Kiaan Castillo <kiaan_castillo@sfu.ca>
2022-03-26 13:02:44 -0700

    Add card flip animation and fixed card height

--- app/src/main/java/com/example/sfuquizlet/StudyDeckFragment.kt
+++ app/src/main/java/com/example/sfuquizlet/StudyDeckFragment.kt
@@ -1,17 +1,19 @@
 package com.example.sfuquizlet
 
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sfuquizlet.database.getCardsFromDatabase
-import com.example.sfuquizlet.database.getUserFromDatabase
 
 const val ARG_ID = "id"
 const val ARG_DEPARTMENT = "department"
@@ -103,26 +105,53 @@ class CardsRecyclerViewAdapter(var cards: MutableList<Card>) : RecyclerView.Adap
         val editButton: ImageButton = view.findViewById<ImageButton>(R.id.edit_button)
         val deleteButton: ImageButton = view.findViewById<ImageButton>(R.id.delete_button)
 
+        private lateinit var card: Card
+
         fun setData(card: Card) {
             // Populate card contents
-            questionTextView.text = card.question
-            authorTextView.text = "Added by ${card.authorId}"
+            this.card = card
+
+            questionTextView.text = this.card.question
+            authorTextView.text = "Added by ${this.card.authorId}"
 
             // Toggle between question and answer
             val view = this.itemView
             view.setOnClickListener {
-                // If displaying question, switch to answer
-                if (isDisplayingQuestion) {
-                    questionTextView.text = card.answer
-
-                // Else, switch to question
-                } else {
-                    questionTextView.text = card.question
-                }
-
-                // Toggle boolean check
-                isDisplayingQuestion = !isDisplayingQuestion
+                // TODO: Update cards viewed count
+                onClickFlipCard()
             }
+        }
+
+        private fun onClickFlipCard() {
+            val view = this.itemView
+            val animation1 = ObjectAnimator.ofFloat(view, "scaleY", 1f, 0f)
+            val animation2 = ObjectAnimator.ofFloat(view, "scaleY", 0f, 1f)
+
+            animation1.interpolator = AccelerateDecelerateInterpolator()
+            animation1.duration = 100
+
+            animation2.interpolator = AccelerateDecelerateInterpolator()
+            animation2.duration = 100
+
+            animation1.addListener(object : AnimatorListenerAdapter() {
+                // Once this animation ends, call the next one
+                override fun onAnimationEnd(animation: Animator?) {
+                    super.onAnimationEnd(animation)
+                    // If displaying question, switch to answer
+                    if (isDisplayingQuestion) {
+                        questionTextView.text = card.answer
+
+                    // Else, switch to question
+                    } else {
+                        questionTextView.text = card.question
+                    }
+
+                    // Toggle boolean check
+                    isDisplayingQuestion = !isDisplayingQuestion
+                    animation2.start()
+                }
+            })
+            animation1.start()
         }
     }
 
